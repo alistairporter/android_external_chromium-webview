e7301eee441ad61ad26691976ae4495ec02f3eeb
Alistair Porter <alistair_porter@outlook.com>
2018-09-15 16:25:15 +0100

    1

--- Android.mk
+++ Android.mk
@@ -23,12 +23,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := webview
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MULTILIB := both
-LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 
-LOCAL_MODULE_TARGET_ARCH := arm arm64 mips x86 x86_64
+LOCAL_MODULE_TARGET_ARCH := arm arm64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/webview.apk
 
